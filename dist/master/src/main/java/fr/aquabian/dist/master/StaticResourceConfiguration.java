ecd5766ac42ddfdd5cb4b685d5caeaba40a2f09c
Amaury LECLERC <amauryleclerc@hotmail.fr>
2018-04-21 15:20:44 +0200

    Ajout config dist

---
+++ dist/master/src/main/java/fr/aquabian/dist/master/StaticResourceConfiguration.java
@@ -0,0 +1,37 @@
+package fr.aquabian.dist.master;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StaticResourceConfiguration implements WebMvcConfigurer {
+
+
+    private static final String[] ROUTER = {
+            "/sprints", "/sprint/**",
+            "/stories", "/story/**", "/plush", "/graph"};
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//
+//        registry.addResourceHandler("swagger-ui.html")//
+//                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")//
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+        registry.addResourceHandler("/img/**")//
+                .addResourceLocations("classpath:/img/");
+
+        registry.addResourceHandler("/**")//
+                .addResourceLocations("classpath:/static/");
+
+        registry.addResourceHandler(ROUTER)//
+                .addResourceLocations("classpath:/static/index.html");
+
+    }
+
+
+}
